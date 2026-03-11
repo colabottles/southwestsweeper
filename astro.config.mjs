058bc01533ee9ec0db91481761b1a0f4caef29b8
Todd Libby <toddlibby@protonmail.com>
2026-03-10 19:48:30 -0700

    fix: ditched the dumpster fire called tailwind for vanilla css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://southwestsweeper.netlify.app",
   output: "static",
   trailingSlash: "ignore",
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
